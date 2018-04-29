8c351989afc32152f0de74bc1a1acd5d27c7bbca
Felix Farias Fueyo <felixfariasfueyo@hotmail.com>
2018-04-29 14:23:03 +0200

    Cross-Section and Inter-Section Methods

--- Python/CDS Proxy Methods/ProxyMethods.py
+++ Python/CDS Proxy Methods/ProxyMethods.py
@@ -10,25 +10,32 @@ import pandas as pd
 
 class ProxyMethods:
     
-    def __init__(self, num_index, index_names, spreadXy):
-        #number of indeces user to divide data set/create buckets (numeric)
+    def __init__(self, num_index, index_names, SpreadXy):
+        #number of indeces used to divide data set/create buckets (numeric)
         self.num_index = num_index
         #name of indeces used to create buckets (vector)
-        self.index_name
-        #spread maturity, where spreadXy = the X year spread. (string 'spread{int}y')
-        self.spreadXy = spreadXy 
+        self.index_names = index_names
+        #spread maturity, where spreadXy = the X year spread. (string 'Spread{int}y')
+        self.spreadXy = SpreadXy 
         
     #Prepare the data set for proxy modeling
-    def pre_process(self, CDS_data, low_rating, currency, simple_regions, no_gov, simple_cat, bps_cap):
+    def pre_process(self, CDS_data, low_rating, rating_spread_nan, currency, simple_regions, no_gov, simple_cat, bps_cap):
         CDS_data = pd.DataFrame(CDS_data)
         
         #Remove CCC and D rated credits. Their spreads considered outliers.
         if low_rating == True:
            CDS_data = CDS_data.loc[np.logical_and(CDS_data['AvRating'] != 'CCC', CDS_data['AvRating'] != 'D')]
+                   
+        #Remove observations with no rating
+        if rating_spread_nan == True:
+            rating_nans = CDS_data[CDS_data['AvRating'].isnull()].index
+            CDS_data = CDS_data.drop(rating_nans)
+            spread_nans = CDS_data[CDS_data[self.spreadXy].isnull()].index
+            CDS_data = CDS_data.drop(spread_nans)
         
         #Retain quotes in currencies according to geographic regions
         if currency == True:
-            eu_countries = ['Albania', 'Andorra', 'Armenia', 'Austria', 'Azerbaijan', 'Belgium','Bulgaria', 'Croatia', 'Cyprus', 'Czech Republic', 'Denmark', 'Finland', 'France', 'Germany', 'Greece', 'Hungary', 'Iceland', 'Ireland', 'Italy','Lithuania','Luxembourg','The Netherlands','Norway','Poland','Portugal','Spain','Sweden','Switzerland','United Kingdom']
+            eu_countries = ['Albania', 'Andorra', 'Armenia', 'Austria', 'Azerbaijan', 'Belgium', 'Bulgaria', 'Croatia', 'Cyprus', 'Czech Republic', 'Denmark', 'Finland', 'France', 'Germany', 'Greece', 'Hungary', 'Iceland', 'Ireland', 'Italy', 'Kazakhstan', 'Lithuania','Luxembourg','The Netherlands','Norway','Poland','Portugal','Spain','Sweden','Switzerland','United Kingdom']
             for i in eu_countries:
                 CDS_data.loc[CDS_data['Country'] == i] = CDS_data.loc[np.logical_and(CDS_data['Country'] == i, CDS_data['Ccy'] == 'EUR')]
         
@@ -47,13 +54,13 @@ class ProxyMethods:
             
         #Redifine the sector categories to a smaller set
         if simple_cat == True:
-            cyclical = ['Basic Materials', 'Consumer Services', 'Energy', 'Technology', 'Telecommunication services', 'Industrials']
+            cyclical = ['Basic Materials', 'Consumer Services', 'Energy', 'Technology', 'Telecommunications Services', 'Industrials']
             for i in cyclical:
-                CDS_data = CDS_data.loc[CDS_data['Sector'] == i] = 'Cyclical'
+                CDS_data.loc[CDS_data['Sector'] == i, 'Sector'] = 'Cyclical'
             
             non_cyclical = ['Consumer Goods', 'Healthcare', 'Utilities']
             for i in non_cyclical:
-                CDS_data = CDS_data.loc[CDS_data['Sector'] == i] = 'Non Cyclical'
+                CDS_data.loc[CDS_data['Sector'] == i, 'Sector'] = 'Non Cyclical'
         
         #Cap the max. spread to 1000 bps.
         if bps_cap == True:
@@ -67,13 +74,78 @@ class ProxyMethods:
         
         return norm_RR
     
-    #Intersection method
-    def intersection_model(self, CDS_data):
-        #Create buckets, e.g. with region, sector and rating as indeces.
-        i_CDS_data = pd.DataFrame(CDS_data).groupby(by = self.index_name)
+    #Intersection method (for pre-processed data)
+    def intersection_method(self, CDS_data, test_set):
+        test_set = test_set.assign( InterEstimate = pd.Series(np.zeros(len(test_set))))
+        
+        #Obtain all different categories of highest hierarchy
+        h_cat = test_set[self.index_names[0]].unique()
+        
+        #Obtain proxy spread for each test observation by looping over the
+        for i in h_cat:
+            #For the given region determine what sectors are in test_set
+            h2_cat = []
+            h2_cat = test_set.loc[test_set[self.index_names[0]] == i, self.index_names[1]].unique()
+            if len(h2_cat) != 0:
+                #Spread for every possible h1, h2 combination
+                for j in h2_cat:
+                    h3_cat = []
+                    h3_cat = test_set.loc[np.logical_and(test_set[self.index_names[0]] == i, test_set[self.index_names[1]] == j), self.index_names[2]].unique()
+                    if len(h3_cat) != 0:
+                        for m in h3_cat:
+                            #Get intersection estimate for test obs. with region = i, ...
+                            s_hat = CDS_data.loc[np.logical_and(np.logical_and(CDS_data[self.index_names[0]] == i, CDS_data[self.index_names[1]] == j), CDS_data[self.index_names[2]] == m), self.spreadXy]
+                            #Assign estimate to test obs. with region = i, ...
+                            test_set.loc[np.logical_and(np.logical_and(test_set[self.index_names[0]] == i, test_set[self.index_names[1]] == j), test_set[self.index_names[2]] == m), 'InterEstimate'] = np.nanmean(s_hat)
+                            
+        return test_set
+    
+    def cross_section_method(self, CDS_data, test_set):
+        test_set = test_set.assign( CrossEstimate = pd.Series(np.zeros(len(test_set))))
+        #Log-transformation of spread
+        CDS_data[self.spreadXy] = np.log(CDS_data[self.spreadXy])
+        
+        #Apply the same algorithm as for intersection method to create buckets and obtain estimate
+        h_cat = test_set[self.index_names[0]].unique()
+
+        for i in h_cat:
+            h2_cat = []
+            h2_cat = test_set.loc[test_set[self.index_names[0]] == i, self.index_names[1]].unique()
+            if len(h2_cat) != 0:
+                for j in h2_cat: 
+                    h3_cat = []
+                    h3_cat = test_set.loc[np.logical_and(test_set[self.index_names[0]] == i, test_set[self.index_names[1]] == j), self.index_names[2]].unique()
+                    if len(h3_cat) != 0:
+                        for m in h3_cat:
+                            s_hat = CDS_data.loc[np.logical_and(np.logical_and(CDS_data[self.index_names[0]] == i, CDS_data[self.index_names[1]] == j), CDS_data[self.index_names[2]] == m), self.spreadXy]
+                            test_set.loc[np.logical_and(np.logical_and(test_set[self.index_names[0]] == i, test_set[self.index_names[1]] == j), test_set[self.index_names[2]] == m), 'CrossEstimate'] = np.nanmean(s_hat)
+
+        test_set['CrossEstimate'] = np.exp(test_set['CrossEstimate'])
+        
+        return test_set
+    
+    #Extended cross-section method with equity, volatility or equity and volatility
+    def equity_method(self, CDS_data, equity_data, test_data, equity_vol_ev):
+        if equity_vol_ev.size == 2:
+            regressors = 'EquityVol'
+        elif equity_vol_ev == 'Equity':
+            regressors = 'Equity'
+        elif equity_vol_ev == 'Vol':
+            regressors = 'Vol'
         
         
+        return test_data
+    
+    def rmse(self, real_value, proxy_estimate):
+        #use self.dVariable and test_set
+        from sklearn.metrics import mean_squared_error
         
+        return np.sqrt(mean_squared_error(real_value, proxy_estimate))
+    
+    def mad(self, real_value, proxy_estimate):
+        'use self.dVariable and test_set'
+        from sklearn.metrics import mean_absolute_error
         
+        return mean_absolute_error(real_value, proxy_estimate)
     
     
--- Python/CDS Proxy Methods/main.py
+++ Python/CDS Proxy Methods/main.py
@@ -9,7 +9,7 @@ Created on Wed Apr 25 00:01:52 2018
 import numpy as np 
 import pandas as pd
 import matplotlib.pyplot as plt
-import datetime
+from ProxyMethods import ProxyMethods
 
 plt.rcParams['figure.figsize'] = (10.0, 8.0)
 import seaborn as sns
@@ -21,11 +21,48 @@ CDS_data = pd.read_csv('Data/CDS_data.csv')
 
 ### Pre-Process ###
 
+#Initialize (Parameter Values)
+num_index = 3
+index_names = ['Region','Sector','AvRating']
+SpreadXy = 'Spread5y'
+proxy_model = ProxyMethods(num_index, index_names, SpreadXy)
 
-### Cross-Section Testing ###
+#Preprocess the data
+CDS_data = proxy_model.pre_process(CDS_data, low_rating = True, rating_spread_nan = True, currency = True, simple_regions = True, no_gov = True, simple_cat = True, bps_cap = True)
 
-#1. Create N data sets.
+#Normalize spread
+recovery_rates = CDS_data['Recovery']
+CDS_data[SpreadXy] = proxy_model.noralize(recovery_rates, CDS_data[SpreadXy])
 
-#2. Apply the methodologies to every possible N-1 data set.
+### Cross-Validation Out-of-Sample ###
 
-#3. Test their accuracy on the 'left-one-out' subsets.
+num_models = ['InterSection', 'CrossSection', 'Equity', 'EquityVol']
+cross_validations = 10
+sample_size = len(CDS_data)/cross_validations
+
+performance_rmse = np.zeros((cross_validations, len(num_models)))
+performance_rmse = pd.DataFrame(performance_rmse)
+performance_rmse.columns = num_models
+performance_rmse.index.names = ['Results']
+for i in range(cross_validations):
+    #1. Create N test and sample sets.
+    test_set = CDS_data[int((i)*sample_size):int((i+1)*sample_size)]
+    CDS_data_temp = CDS_data.drop(test_set.index)
+    
+    #2. Apply the methodologies to every possible N-1 data set.
+    test_set = proxy_model.intersection_method(CDS_data_temp, test_set)
+    test_set = proxy_model.cross_section_method(CDS_data_temp, test_set)
+
+    #3. Test their accuracy on the 'left-one-out' subsets.
+    nans1 = test_set[test_set['InterEstimate'].isnull()].index
+    test_set1 = test_set.drop(nans1)
+    intersection_rmse = proxy_model.rmse(test_set1[SpreadXy], test_set1['InterEstimate'])
+    performance_rmse.loc[performance_rmse.index == i, 'InterSection'] = intersection_rmse
+    
+    nans2 = test_set[test_set['CrossEstimate'].isnull()].index
+    test_set2 = test_set.drop(nans2)
+    crosssection_rmse = proxy_model.rmse(test_set2[SpreadXy], test_set2['CrossEstimate'])
+    performance_rmse.loc[performance_rmse.index == i, 'CrossSection'] = crosssection_rmse
+    
+inter_rmse = np.mean(performance_rmse['InterSection'])
+cross_rmse = np.mean(performance_rmse['CrossSection'])
